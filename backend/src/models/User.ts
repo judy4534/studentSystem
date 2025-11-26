fc429e708cfd50d70d06bf7b0f3b35a41c7d65c1
judy4534 <judysalem0412@gmail.com>
2025-11-26 22:02:42 +0200

    Refactor User model and remove unused imports
    Signed-off-by: judy4534 <judysalem0412@gmail.com>

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,9 +1,8 @@
-
-import mongoose, { HookNextFunction } from 'mongoose';
+import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import { IUser } from '../types/models';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
         required: true,
@@ -16,7 +15,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        select: false, // Do not return password by default
+        select: false,
     },
     role: {
         type: String,
@@ -26,7 +25,7 @@ const userSchema = new mongoose.Schema({
     studentId: {
         type: String,
         unique: true,
-        sparse: true, // Allows multiple nulls
+        sparse: true,
     },
     department: {
         type: mongoose.Schema.Types.ObjectId,
@@ -41,21 +40,23 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 });
 
-// Password hashing middleware
-// FIX: Use the generic form of `.pre<IUser>()` to correctly type `this` and allow access to Mongoose document methods like `isModified`.
-userSchema.pre<IUser>('save', async function (next: HookNextFunction) {
+// ✔ FIX 1: حذف HookNextFunction نهائيًا (مو موجود في Mongoose v7)
+userSchema.pre('save', async function (this: IUser, next) {
     if (!this.isModified('password')) {
         return next();
     }
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+
     next();
 });
 
-// Method to compare passwords
+// ✔ FIX 2: matchPassword تعمل بدون مشاكل
 userSchema.methods.matchPassword = async function (this: IUser, enteredPassword: string): Promise<boolean> {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model<IUser>('User', userSchema);
+
 export default User;
